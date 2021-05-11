0413d7b74f8815cf080c954f9e344fe703fe08de
jokeejoke <69756862+jokeejoke@users.noreply.github.com>
2021-05-11 18:58:23 +0600

    add full index page

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,36 @@
-//import $ from 'jquery'; window.jQuery = $; window.$ = $ // import module example (npm i -D jquery)
+import $ from 'jquery'; window.jQuery = $; window.$ = $ // import module example (npm i -D jquery)
+import Swiper, {
+	Autoplay,
+	Pagination,
+	Navigation
+} from 'swiper';
+import magnificPopup from 'magnific-popup';
 
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  $('.open-popup').magnificPopup({
+    type:'inline',
+    removalDelay: 300,
+    mainClass: 'mfp-fade',
+    showCloseBtn: false,
+    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  });
+  $(".form-popup-close").on("click", function() {
+    $.magnificPopup.close()
+  })
+
+  $(window).scroll(function() {
+		if ($(this).scrollTop() > $(this).height()) {
+			$('.to-top').addClass('active')
+		} else {
+			$('.to-top').removeClass('active')
+		}
+	})
+
+  $('.to-top').on('click', function() {
+		$('html, body').stop().animate({ scrollTop: 0}, 'slow', 'swing')
+	})
 
 	function showContacts () {
     const $icon = document.querySelector(".contacts-mobile-icon");
@@ -41,9 +70,49 @@ document.addEventListener('DOMContentLoaded', () => {
 		})
 	}
 
+  // Init Slider
+  Swiper.use([Autoplay, Pagination, Navigation]);
+
+  function initSlider (selector, paginationSelector, loop = false) {
+    var slider = new Swiper(selector, {
+      slidesPerView: 3,
+      spaceBetween: 5,
+      speed: 800,
+      freeMode: true,
+      loop: false,
+      //centeredSlides: "auto",
+      pagination: {
+        el: paginationSelector,
+        clickable: true,
+        renderBullet: function (index, className) {
+          if (index < 9) {
+            index = '0' + (index + 1);
+          }
+          return '<span class="' + className + '">' + (index) + "</span>";
+        },
+      },
+      navigation: {
+        nextEl: ".slider-carousel__next",
+        prevEl: ".slider-carousel__prev",
+      },
+      breakpoints: {
+        992: {
+          loop: loop,
+          spaceBetween: 20,
+        },
+        768: {
+          spaceBetween: 20,
+        }
+      }
+    });
+  }
+  
 
   showContacts();
   hamburgerMenu();
+  initSlider(".office-slider .slider-carousel", ".office-slider .slider-carousel-pagination");
+  initSlider(".plumbing-slider .slider-carousel", ".plumbing-slider .slider-carousel-pagination", true);
+  initSlider(".interroom-slider .slider-carousel", ".interroom-slider .slider-carousel-pagination", true);
   
   // Запускаем функции в десктопах и на мобильных устройствах
   if (window.innerWidth <= 992) {
@@ -53,5 +122,9 @@ document.addEventListener('DOMContentLoaded', () => {
   if (window.innerWidth <= 768) {
     document.querySelector(".main-section-btn").innerHTML = "Рассчитать стоимость";
   }
+
+  window.addEventListener("resize", function() {
+    //location.reload();
+  })
 
 })
